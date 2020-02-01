99d0a16ad72a80a8ac622a146fae04588a527542
YujiaY <jack.yujia.yuan@gmail.com>
2020-02-01 22:39:55 +1000

    Add User model, register and login routes.

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const colors = require('colors');
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
 
-const { connectDB } = require('./config/db');
-const errorHandler = require('./middleware/errorHandler');
+const { connectDB } = require("./config/db");
+const errorHandler = require("./middleware/errorHandler");
 
 //Load env vars
-dotenv.config({path: './config/config.env'});
+dotenv.config({ path: "./config/config.env" });
 
 //Routes files
-const bootcamps = require('./routes/bootcamps');
-const courses = require('./routes/courses');
+const auth = require("./routes/auth");
+const bootcamps = require("./routes/bootcamps");
+const courses = require("./routes/courses");
 
 const app = express();
 
@@ -19,37 +20,37 @@ const app = express();
 app.use(express.json());
 
 // Dev logging middleware
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 //Mount routers
-app.use('/api/v1/bootcamps', bootcamps);
-app.use('/api/v1/courses', courses);
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/bootcamps", bootcamps);
+app.use("/api/v1/courses", courses);
 
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5001;
 
 connectDB()
-  .then((res) => {
+  .then(res => {
     console.log(`DB Connected to ${res.connections[0].host}`.yellow.bgGreen);
     const server = app.listen(
       PORT,
       console.log(
-        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}.`
-          .green
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}.`.green
       )
     );
     // Handle unhandled promise rejections
-    process.on('unhandledRejection', (err, promise) => {
+    process.on("unhandledRejection", (err, promise) => {
       console.log(`Error: ${err.message}`.red);
       // Close server and exit process
       server.close(() => process.exit(1));
     });
   })
-  .catch((e) => {
-  console.log('DB Connection Failed!'.red);
-  console.error(e.message);
-  process.exit(1);
-});
+  .catch(e => {
+    console.log("DB Connection Failed!".red);
+    console.error(e.message);
+    process.exit(1);
+  });
