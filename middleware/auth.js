7e7bc820fd4767aca215a717c70829becbd7ee83
YujiaY <jack.yujia.yuan@gmail.com>
2020-02-05 19:27:31 +1000

    Use the token from cookie.

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,12 +10,12 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
+    // Check token from Bearer token in header
     token = req.headers.authorization.split(" ")[1];
+    // Check token from cookie
+  } else if (req.cookies.token) {
+    token = req.cookies.token;
   }
-
-  // else if (req.cookies.token) {
-  //   token = req.cookies.token;
-  // }
 
   // Make sure token exists
   if (!token) {
